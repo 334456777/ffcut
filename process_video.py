d94b0f3b2e130e8faf64b2a3fbbbce303eabb800
334456777 <yusteven2004@gmail.com>
2024-07-05 19:40:36 +0900

    Update process_video.py

--- process_video.py
+++ process_video.py
@@ -9,12 +9,8 @@ fi
 input_video="$1"
 output_video="output.mp4"
 
-# 步骤: 去除静音部分
 ffmpeg -i "$input_video" -af "silenceremove=stop_periods=-1:stop_threshold=-40dB:detection=peak" -c:v copy -c:a aac -strict experimental "$output_video"
 if [ $? -ne 0 ]; then
     echo "Error: Failed to remove silence."
     exit 1
 fi
-
-echo "处理完成："
-echo "视频文件: $output_video"
