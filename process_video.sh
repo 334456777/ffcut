b4e875df60ed0788ae8a6d20639aa3bc92a3e9ee
334456777 <yusteven2004@gmail.com>
2024-07-05 21:42:23 +0900

    Update process_video.sh

--- process_video.sh
+++ process_video.sh
@@ -3,7 +3,7 @@
 input_file=$1
 output_file="output.mp4"
 silence_threshold=-40dB
-silence_duration=0.5
+silence_duration=1
 
 # Step 1: Detect silence and save the log
 ffmpeg -i "$input_file" -af silencedetect=n=$silence_threshold:d=$silence_duration -f null - 2> silence_log.txt
