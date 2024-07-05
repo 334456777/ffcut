6e97efd8ee4e846bb2b8fdc4ccf4e61e8143286f
334456777 <yusteven2004@gmail.com>
2024-07-05 18:24:47 +0900

    Update process_video.sh

--- process_video.sh
+++ process_video.sh
@@ -8,7 +8,7 @@ fi
 
 input_video="$1"
 
-ffmpeg -i input.mp4 -af silenceremove=stop_periods=-1:stop_duration=0.3:stop_threshold=-30dB -y output.mp4
+ffmpeg -i input.mp4 -af silenceremove=stop_periods=-1:stop_duration=0.000001:stop_threshold=-30dB -y output.mp4
 if [ $? -ne 0 ]; then
     echo "Error: Failed to remove silence."
     exit 1
