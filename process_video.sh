23bd1a9a1fd4608916682f500dde64226dc9a4ad
334456777 <yusteven2004@gmail.com>
2024-07-05 18:05:25 +0900

    Update process_video.sh

--- process_video.sh
+++ process_video.sh
@@ -7,10 +7,8 @@ if [ "$#" -ne 1 ]; then
 fi
 
 input_video="$1"
-output_video="output.mp4"
 
-# 使用复杂过滤器去除静音片段并合并剩下的部分
-ffmpeg -i "$input_video" -vf "select='gt(scene,0.4)',setpts=N/FRAME_RATE/TB" -af "silenceremove=stop_periods=-1:stop_threshold=40dB:detection=peak,aresample=async=1" -c:v copy -c:a copy "$output_video"
+ffmpeg -i input.mp4 -af silenceremove=stop_periods=-1:stop_duration=0.3:stop_threshold=-30dB -y output.mp4
 if [ $? -ne 0 ]; then
     echo "Error: Failed to remove silence."
     exit 1
